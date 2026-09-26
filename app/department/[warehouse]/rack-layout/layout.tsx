f8d85f536c6abd52365bc71f2d0629682b6a0101
nska777 <60347233+nska777@users.noreply.github.com>
2026-09-26 09:21:47 +0500

    hide 3D camera toolbar on paint rack layout

---
+++ app/department/[warehouse]/rack-layout/layout.tsx
@@ -0,0 +1,5 @@
+import "./rack-layout.css";
+
+export default function PaintRackLayout({ children }: { children: React.ReactNode }) {
+  return <div className="paint-rack-layout-page">{children}</div>;
+}
